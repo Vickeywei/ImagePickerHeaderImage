53f34349c2015182cbac6b64f5f6df934de5af8a
wei qi <weiqi@hzdracom.com>
2016-08-08 17:08:07 +0800

    Initial Commit

---
+++ ImagePickerViewController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImagePickerViewController
+//
+//  Created by 魏琦 on 16/8/8.
+//  Copyright © 2016年 com.drcacom.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ImagePickerViewController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImagePickerViewController
+//
+//  Created by 魏琦 on 16/8/8.
+//  Copyright © 2016年 com.drcacom.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
